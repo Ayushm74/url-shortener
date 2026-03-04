6505cdcedefa1f6ba199efbc12963c8a033c9af2
Ayushm74 <Ayushmishra74992@gmail.com>
2026-03-04 17:32:16 +0530

    update

--- models/Url.js
+++ models/Url.js
@@ -1,10 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+/*
+Schema to store original URL, short code,
+number of clicks and creation date.
+*/
 
 const UrlSchema = new mongoose.Schema({
-  fullUrl: { type: String, required: true },
-  shortCode: { type: String, required: true, unique: true },
-  clicks: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now }
+    originalUrl: {
+        type: String,
+        required: true
+    },
+    shortCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    clicks: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-module.exports = mongoose.model('Url', UrlSchema);
+module.exports = mongoose.model("Url", UrlSchema);
